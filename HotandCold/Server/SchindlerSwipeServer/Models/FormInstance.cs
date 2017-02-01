64bccb6752d3c537bc1b83fed9319a07e59a88dc
RazSinay <raz.sinay@gmail.com>
2017-02-01 21:33:14 +0200

    German server

---
+++ HotandCold/Server/SchindlerSwipeServer/Models/FormInstance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchindlerSwipeServer.Models
+{
+    public class FormInstance
+    {
+        public int ID { get; set; }
+        public FormTemplate FormTemplate { get; set; }
+        public User User { get; set; }
+        public int FlowInstanceId { get; set; }
+    }
+}
