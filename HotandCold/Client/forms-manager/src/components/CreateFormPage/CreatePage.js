af953ab993977697e19ce584ec7e3e5066563444
moshekathz <moshek777@gmail.com>
2017-02-01 22:30:12 +0200

    bla

--- HotandCold/Client/forms-manager/src/components/CreateFormPage/CreatePage.js
+++ HotandCold/Client/forms-manager/src/components/CreateFormPage/CreatePage.js
@@ -4,13 +4,52 @@ var React = require('react');
 var Router = require('react-router');
 var Link = Router.Link;
 var FormTemplate = require('./FormTemplate');
+var toastr = require('toastr');
 
 var CreatePage = React.createClass({
+    getInitialState: function () {
+        return {
+            formFields: [
+                { name: "Moshe", type: "text"}
+            ]
+        };
+    },
+
+    addTextBox: function() {
+        var fieldName = prompt("please enter your field's name:");
+        this.state.formFields.push({ name: fieldName, type: 'text' });
+        this.setState({formFields: this.state.formFields});
+        toastr.success('Textbox added succesfully!');
+    },
+
+    addNumberPicker: function() {
+        var fieldName = prompt("please enter your field's name:");
+        this.state.formFields.push({ name: fieldName, type: 'number' });
+        this.setState({formFields: this.state.formFields});
+        toastr.success('Number Picker added!');
+    },
+
+    getFormName: function() {
+        var formName = prompt("please enter your form's name:");
+    },
+
+    saveForm: function() {
+        toastr.success('Form saved!');
+    },
+
     render: function() {
         return (
             <div className="jumbotron">
                 <h1>Create A Form</h1>
-                <FormTemplate />
+                <br/>
+                <button className="btn btn-primary btn-lg" onClick={this.getFormName}>Create!</button>
+
+                <FormTemplate name="my form" formFields={this.state.formFields}/><br/>
+
+                <button className="btn dropup" onClick={this.addTextBox}>Textbox</button> 
+                <button className="btn btn-primary btn-md" onClick={this.addNumberPicker}>NumberPicker</button>
+                <br/><br/>
+                <button className="btn dropdown-menu-right" onClick={this.saveForm}>Save</button>
             </div>
         );
     }
--- HotandCold/Client/forms-manager/src/components/CreateFormPage/FormTemplate.js
+++ HotandCold/Client/forms-manager/src/components/CreateFormPage/FormTemplate.js
@@ -5,39 +5,26 @@ var TextInput = require('../common/textInput');
 
 var FormTemplate = React.createClass({
     PropTypes: {
-        author: React.PropTypes.object.isRequired,
-        onSave: React.PropTypes.func.isRequired,
-        onChange: React.PropTypes.func.isRequired,
-        errors: React.PropTypes.object
+        name: React.PropTypes.string.isRequired,
+        formFields: React.PropTypes.object.isRequired
     },
 
-    getInitialState: function() {
-        return {
-            formFields: [
-                { id: '1', name: 'moshe', type: 'text' }
-            ]
-        };
-    },
-
-    addTextBox: function() {
-        this.state.formFields.push({ id: '2', name: 'koren', type: 'text' });
-        this.setState({formFields: this.state.formFields});
+    createFields: function(field, i) {
+        return (
+            <div key={i}>
+                <label>{field.name}: </label>
+                <input type={field.type}/>
+                <br/>
+            </div>
+        );
     },
 
     render: function () {
-        var fieldsToRender = this.state.formFields.map((item) => (
-            <div>
-                <label>{item.name}: </label>
-                <input key={item.id} type={item.type}/>
-            </div>
-        ))
-
         return(
             <form>
-                <h1>My form</h1>
-                {fieldsToRender}
+                <h2>Tofes Neshek</h2>
                 <br/>
-                <button onClick={this.addTextBox}>Click Me</button>
+                {this.props.formFields.map(this.createFields)}
             </form>
         );
     }
